c01f24f272929955d02f79a275c7d5dd18c39cf2
Maria Paula Rojas Castro -X (mrojasca - NUB78 SAS at Cisco) <mrojasca@cisco.com>
2022-09-28 08:13:21 -0500

    CR V.1.0
    Sharepoint name: CR_Template_V.1.0
    TAC/ SR Creation date filter: Filtered by date and not by quarter.

--- utils_lib.py
+++ utils_lib.py
@@ -519,9 +519,7 @@ def get_ib_data(user, ids, id_type):
     cs.close()
     cnn.close()
     
-    ib_df = pd.DataFrame(df)
-
-    ib_df.columns = ['customer_id', 
+    ib_df_columns = ['customer_id', 
                     'customer_name',
                     'Best Site Sales Level 2 Name', 
                     'Coverage', 
@@ -642,6 +640,10 @@ def get_ib_data(user, ids, id_type):
                     'ADJUSTED_CATEGORY',
                     'Service BE GEO Name',]
 
+    
+    ib_df = pd.DataFrame(df, columns=ib_df_columns)
+
+    
     ib_df[['L12HR','L12OS',
                 'L14HR',
                 'L14OS',
